def56bf22d66f8cdafe0c5080bf862aeff7fe5c8
danilocbueno <danilocbueno@gmail.com>
2022-06-01 11:49:11 -0300

    firts commit

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>pages / index</h1>
+</template>
---
+++ pages/membros.vue
@@ -0,0 +1,23 @@
+<script setup>
+const { path } = useRoute();
+const { data } = await useAsyncData("content", () =>
+  queryContent(path).only(["_path", "title"]).find()
+);
+</script>
+<template>
+  <main>
+    <div v-for="link of data" :key="link._path">
+      <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
+    </div>
+
+    <pre>
+    {{ path }}
+  </pre
+    >
+
+    <pre>
+    {{ data }}
+  </pre
+    >
+  </main>
+</template>
